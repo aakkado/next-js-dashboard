4486b1d013e447919827abdaffdb544bbf4cfb7d
aakkado <liltenshidesu@gmail.com>
2024-03-18 15:09:47 -0300

    Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,12 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { Suspense } from 'react'; 
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata : Metadata = {
+    title: 'Invoice ',
+};
 
 import { 
     RevenueChartSkeleton,
